d25ad9608a67758e215ad5ec08cc6db71abc934b
Alkivi <admin@alkivi.fr>
2014-08-11 15:05:29 +0200

    added support helper

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -46,4 +46,16 @@ class alkivi_base::scripts () {
     target  => '/root/alkivi-scripts/gensecuredata',
     require => File['/root/alkivi-scripts/gensecuredata'],
   }
+
+  # support helper
+  file { '/root/alkivi-scripts/support':
+    source  => 'puppet:///modules/alkivi_base/scripts-helpers/support',
+    require => File['/root/alkivi-scripts/'],
+  }
+
+  file { '/usr/bin/support':
+    ensure  => link,
+    target  => '/root/alkivi-scripts/support',
+    require => File['/root/alkivi-scripts/support'],
+  }
 }
